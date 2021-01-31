c49e6b37c0832784b349cf4095d9ca761bd96c8e
Michael Markey <michaelamarkey@gmail.com>
2021-01-31 20:16:12 +0900

    add framing for repos

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import { Header, Main } from './semantic'
 
 function App() {
--- src/components/Repos.tsx
+++ src/components/Repos.tsx
@@ -1,8 +1,50 @@
-import React from 'react'
+/** @jsx jsx */
+import { jsx } from '@emotion/react'
+
 import Article from '../semantic/Article'
 
+interface Repo {
+  id: string
+  description: string
+  name: string
+}
+
+const data: Repo[] = [
+  { id: '1', name: 'repo 1', description: 'cool repo' },
+  { id: '2', name: 'repo 2', description: 'cool repo again' }
+]
+
 function Repos() {
-  return <Article>This are my repos!</Article>
+  return (
+    <Article header='My projects'>
+      {data.map(({ id, name, description }) => (
+        <div
+          key={id}
+          css={{
+            marginBottom: 8,
+            padding: 8,
+            border: '1px solid #131516',
+            borderRadius: 4
+          }}
+        >
+          <p
+            css={{
+              fontSize: 16
+            }}
+          >
+            {name}
+          </p>
+          <p
+            css={{
+              fontSize: 14
+            }}
+          >
+            {description}
+          </p>
+        </div>
+      ))}
+    </Article>
+  )
 }
 
 export default Repos
